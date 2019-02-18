ee3f12517da65a75117f2e9f6510bfd1bca41a88
POKINBKK <mr.pokiloli@gmail.com>
2019-02-18 11:04:40 +0700

    dropdown-datepicker-image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './ui/second_screen.dart';
+import './ui/first_screen.dart';
 import './ui/my_custom_form.dart';
 
 void main() => runApp(MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       //home: MyHomePage(),
       initialRoute: "/",
       routes: {
-        "/": (context) => MyCustomForm(),
+        "/": (context) => FirstScreen(),
         "/second": (context) => SecondScreen(),
       }
     );
--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -1,25 +1,101 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import './detail_screen.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return FirstScreenState();
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("First Screen"),
+  //     ),
+  //     body: Center(
+  //       child: FlatButton(
+  //         child: Text("Go to Second"),
+  //         onPressed: (){
+  //           Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //           builder: (context) => DetailScreen(title: "Hello"),
+  //           ),
+  //           );
+  //         },
+  //       )
+  //     ),
+  //   );
+  // }
+
+}
+
+class FirstScreenState extends State {
+  List<String> _passengers = <String>["", "1", "2", "3", "4", "5"];
+  String _passenger = "";
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+          _image = image;
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: (){
+          getImage();
+        },
+      ),
       appBar: AppBar(
         title: Text("First Screen"),
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text("Go to Second"),
-          onPressed: (){
-            Navigator.push(
-            context,
-            MaterialPageRoute(
-            builder: (context) => DetailScreen(title: "Hello"),
+      body: Form(
+        child: ListView(
+        children: <Widget>[
+          _image == null ? Text("No Image selected") : Image.file(_image),
+          InputDecorator(
+            decoration: InputDecoration(
+              labelText: "YO"
             ),
-            );
-          },
-        )
+            isEmpty: _passenger == "",
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                value: _passenger,
+                onChanged: (value){
+                  setState(() {
+                    _passenger = value;
+                  });
+                },
+                items: _passengers.map((String value){
+                  return DropdownMenuItem <String>(
+                    child: Text(value),
+                    value: value,
+                  );
+                }
+                ).toList(),
+              ),
+            ),
+          ),
+          DateTimePickerFormField(
+            inputType: InputType.date,
+            format: DateFormat("yyyy-MM-dd"),
+            decoration: InputDecoration(labelText: "Date"),
+          ),
+
+        ],
+        ),
       ),
     );
   }
